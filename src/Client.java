906a08d16e3e8f5367e113c7bb2e4445b59124fa
maolintuntun <diyeqx@gmail.com>
2020-10-13 18:08:01 +0800

    up

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,10 @@
 
+// 分别测试饿汉和懒汉模式
 public class Client {
-
+	public static void main(String[] args) {
+		
+		System.out.println("this is singleton main");
+		System.out.println("this is " + SingleTonA.getInstance());
+		System.out.println("this is" + SingleTonB.getInstance());
+	}
 }
